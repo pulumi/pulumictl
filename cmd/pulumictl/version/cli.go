8ae62e6726423d7451b3088d1cc26b11e8340c70
Lee Briggs <lee@leebriggs.co.uk>
2020-05-11 18:51:27 -0700

    Run goimports

--- cmd/pulumictl/version/cli.go
+++ cmd/pulumictl/version/cli.go
@@ -2,20 +2,21 @@ package version
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/pulumi/pulumictl/pkg/gitversion"
 	"github.com/pulumi/pulumictl/pkg/version"
 	"github.com/spf13/cobra"
-	"os"
 )
 
-func Command() *cobra.Command{
+func Command() *cobra.Command {
 	command := &cobra.Command{
-		Use: "get-version",
+		Use:   "get-version",
 		Short: "Get the current version",
-		Long: `Get the current version of pulumictl`,
+		Long:  `Get the current version of pulumictl`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			
+
 			v := version.Version
 			// If we haven't set a version with linker flags, use this tool to get the version
 			if v == "" {
@@ -28,13 +29,12 @@ func Command() *cobra.Command{
 				if err != nil {
 					return fmt.Errorf("error calculating version: %w", err)
 				}
-				
+
 				v = version.SemVer
 			}
 			fmt.Println(v)
 			return nil
 		},
-
 	}
 	return command
 }
