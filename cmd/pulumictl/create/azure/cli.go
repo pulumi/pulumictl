448978a7e86202776fcb486a22b7c43699ad623d
stack72 <public@paulstack.co.uk>
2020-09-17 22:30:26 +0100

    Adding a repository dispatch job for publishing the azure provider SDK files

---
+++ cmd/pulumictl/create/azure/cli.go
@@ -0,0 +1,101 @@
+package azure
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/blang/semver"
+	"github.com/google/go-github/v32/github"
+	gh "github.com/pulumi/pulumictl/pkg/github"
+	"github.com/pulumi/pulumictl/pkg/gitversion"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	githubToken string
+	org         string
+	repo        string
+	ref         string
+	tokenClient *http.Client
+)
+
+type Payload struct {
+	Ref string `json:"ref"`
+}
+
+const eventType = "oss-sdk"
+
+func Command() *cobra.Command {
+	command := &cobra.Command{
+		Use:   "oss-sdk [gitRef]",
+		Short: "Publish the Azure Nextgen Provider SDK",
+		Long:  `Send a repository dispatch payload to the pulumi-azure-nextgen repo that triggers the publishing of the SDK`,
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			// Grab all the configuration variables
+			githubToken = viper.GetString("token")
+			org = viper.GetString("org")
+			ref = viper.GetString("ref")
+			containerRepo := "pulumi/pulumi-azure-nextgen"
+			ref := args[0]
+
+			// perform some string manipulation and validation
+			containerRepoArray := strings.Split(containerRepo, "/")
+
+			// if the string split doesn't return 2 values, it's probably not right
+			if len(containerRepoArray) != 2 {
+				return fmt.Errorf("unable to use container repo: format must be <org>/<repo> - value: %s\n", containerRepo)
+			}
+
+			_, err := semver.Parse(gitversion.StripModuleTagPrefixes(ref))
+
+			if err != nil {
+				return fmt.Errorf("must specify a valid semver ref - value: %s\n", ref)
+			}
+
+			// create a github client and token
+			ctx, client := gh.CreateGithubClient(githubToken)
+
+			// create the JSON payload
+			jsonPayload, err := json.Marshal(Payload{
+				Ref: ref,
+			})
+
+			if err != nil {
+				return err
+			}
+			payload := json.RawMessage(jsonPayload)
+
+			// create the repository dispatch event
+			_, _, err = client.Repositories.Dispatch(ctx,
+				containerRepoArray[0],
+				containerRepoArray[1],
+				github.DispatchRequestOptions{
+					EventType:     eventType,
+					ClientPayload: &payload,
+				})
+
+			if err != nil {
+				return fmt.Errorf("unable to create dispatch event: %w\n", err)
+			}
+
+			// output stuff
+			fmt.Println("Submitting dispatch event to:", containerRepo)
+			fmt.Println(string(payload))
+
+			return nil
+
+		},
+	}
+
+	command.Flags().StringP("org", "o", "pulumi", "the GitHub org that hosts the provider in the arg")
+
+	viper.BindEnv("org", "GITHUB_ORG")
+	viper.BindPFlag("org", command.Flags().Lookup("org"))
+
+	return command
+}
--- cmd/pulumictl/create/cli.go
+++ cmd/pulumictl/create/cli.go
@@ -1,6 +1,7 @@
 package create
 
 import (
+	"github.com/pulumi/pulumictl/cmd/pulumictl/create/azure"
 	"github.com/pulumi/pulumictl/cmd/pulumictl/create/chocolatey"
 	"github.com/pulumi/pulumictl/cmd/pulumictl/create/containers"
 	docsbuild "github.com/pulumi/pulumictl/cmd/pulumictl/create/docs-build"
@@ -19,6 +20,7 @@ func Command() *cobra.Command {
 	command.AddCommand(containers.Command())
 	command.AddCommand(chocolatey.Command())
 	command.AddCommand(homebrew.Command())
+	command.AddCommand(azure.Command())
 
 	return command
 }
