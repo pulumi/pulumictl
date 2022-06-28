fc811a0cb57795ee001b155e24b1bab9aa009822
Paul Stack <public@paulstack.co.uk>
2022-06-28 18:06:49 +0100

    Adding a command that will download a binary to a specified location (#48)

---
+++ cmd/pulumictl/download-binary/cli.go
@@ -0,0 +1,151 @@
+// Copyright 2016-2021, Pulumi Corporation.
+//
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package download_binary
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/cmdutil"
+	"github.com/spf13/cobra"
+)
+
+func Command() *cobra.Command {
+	var repoSlug string
+	var name string
+	var version string
+	var host string
+	short := "Downloads a version of a specific binary"
+	cmd := &cobra.Command{
+		Use:     "download-binary",
+		Short:   short,
+		Example: "pulumictl download-binary -n my-binary -r pulumi/pulumi-mybinary -v v0.0.1",
+		Long: short + "\n" +
+			"\nThis will download a version of binary to a specific location.",
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
+			filename := fmt.Sprintf("%s-%s-%s-%s.tar.gz", name, version, runtime.GOOS, runtime.GOARCH)
+			downloadUrl := fmt.Sprintf("%s/%s/releases/download/%s/%s", host, repoSlug, version, filename)
+
+			cwd, err := os.Getwd()
+			if err != nil {
+				return fmt.Errorf("unablle to detect current working directory")
+			}
+			srcFile := fmt.Sprintf("%s/bin/%s", cwd, filename)
+
+			err = downloadBinary(downloadUrl, srcFile)
+			if err != nil {
+				return err
+			}
+
+			err = decompressBinary(srcFile, cwd)
+
+			return os.Remove(srcFile)
+		}),
+	}
+	cmd.PersistentFlags().StringVarP(&repoSlug,
+		"repo-slug", "r", "", "the repo slug for the repository where the binary lives")
+	cmd.PersistentFlags().StringVarP(&name,
+		"name", "n", "", "the name of name to download e.g. pulumi-language-java")
+	cmd.PersistentFlags().StringVarP(&version,
+		"version", "v", "", "the version of the binary e.g. v0.4.0")
+	cmd.PersistentFlags().StringVar(&host,
+		"host", "https://github.com", "The host of the repo to download from. "+
+			"Defaults to https://github.com")
+
+	cmd.MarkFlagRequired("repo-slug")
+	cmd.MarkFlagRequired("name")
+	cmd.MarkFlagRequired("version")
+
+	return cmd
+}
+
+func downloadBinary(url, destFile string) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode < 200 || response.StatusCode > 299 {
+		return fmt.Errorf("unable to find URL: %q, received non 200 response code: %v", url, response.StatusCode)
+	}
+
+	out, err := os.Create(destFile)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Writer the body to file
+	_, err = io.Copy(out, response.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func decompressBinary(srcFile, cwd string) error {
+	reader, err := os.Open(srcFile)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
+	archive, err := gzip.NewReader(reader)
+	if err != nil {
+		return err
+	}
+	defer archive.Close()
+
+	tarReader := tar.NewReader(archive)
+
+	for {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+		path := filepath.Join(fmt.Sprintf("%s/bin", cwd), header.Name)
+		info := header.FileInfo()
+		if info.IsDir() {
+			if err = os.MkdirAll(path, info.Mode()); err != nil {
+				return err
+			}
+			continue
+		}
+
+		file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(file, tarReader)
+		if err != nil {
+			file.Close()
+			return err
+		}
+		file.Close()
+	}
+
+	return nil
+}
